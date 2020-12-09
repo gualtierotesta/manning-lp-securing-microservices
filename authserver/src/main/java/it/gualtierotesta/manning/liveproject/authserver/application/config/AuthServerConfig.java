623656df45f33618d9254cf21224c151a1f26ce4
Gualtiero Testa <gualtiero.testa@pm.me>
2020-12-09 21:22:46 +0100

    Added JWT configuration

--- authserver/src/main/java/it/gualtierotesta/manning/liveproject/authserver/application/config/AuthServerConfig.java
+++ authserver/src/main/java/it/gualtierotesta/manning/liveproject/authserver/application/config/AuthServerConfig.java
@@ -1,12 +1,17 @@
 package it.gualtierotesta.manning.liveproject.authserver.application.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 /**
  * OAuth authorization server configuration
@@ -18,12 +23,29 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @RequiredArgsConstructor
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Value("${jwt.key}")
+    private String jwtKey;
+
     private final AuthenticationManager authenticationManager;
 
     @Override
-    public void configure(
-            final AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        var converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtKey);
+        return converter;
     }
 
     @Override
@@ -32,7 +54,7 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         clients.inMemory()
                 .withClient("client")
                 .secret("secret")
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .scopes("read");
     }
 
