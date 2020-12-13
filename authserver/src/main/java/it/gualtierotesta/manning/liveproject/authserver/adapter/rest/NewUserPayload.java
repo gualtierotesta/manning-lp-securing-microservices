c5747e9425e71aff09b0906873dde630b54e6d3c
Gualtiero Testa <gualtiero.testa@factor-y.com>
2020-12-13 15:12:08 +0100

    Milestone 1.2: first part

---
+++ authserver/src/main/java/it/gualtierotesta/manning/liveproject/authserver/adapter/rest/NewUserPayload.java
@@ -0,0 +1,20 @@
+package it.gualtierotesta.manning.liveproject.authserver.adapter.rest;
+
+import it.gualtierotesta.manning.liveproject.authserver.domain.User;
+import lombok.Data;
+
+@Data
+class NewUserPayload {
+
+    private String username;
+    private String password;
+    private String authority;
+
+    User map() {
+        return User.builder()
+            .username(username)
+            .password(password)
+            .authority(authority)
+            .build()  ;
+    }
+}
---
+++ authserver/src/main/java/it/gualtierotesta/manning/liveproject/authserver/adapter/security/SecurityUser.java
@@ -0,0 +1,52 @@
+package it.gualtierotesta.manning.liveproject.authserver.adapter.security;
+
+import it.gualtierotesta.manning.liveproject.authserver.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    public SecurityUser(final User pUser) {
+        this.user = pUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of( () -> user.getAuthority());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
