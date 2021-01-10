a317aa7623823aa200845303705c24104480a514
Gualtiero Testa <gualtiero.testa@factor-y.com>
2021-01-10 12:30:40 +0100

    Added Spring Cloud Contract to test routes with Wiremock

---
+++ gateway/src/test/java/it/gualtierotesta/manning/liveproject/gateway/RoutesTest.java
@@ -0,0 +1,56 @@
+package it.gualtierotesta.manning.liveproject.gateway;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.mockJwt;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {"app.businessserver.uri=http://locahost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
+@AutoConfigureWebTestClient
+class RoutesTest {
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test
+    @DisplayName("setup")
+    void test1() {
+        assertThat(webClient).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Unauthenticated route profiles get unauthenticated")
+    void test2() {
+        webClient
+            .get()
+            .uri("/profile/user123")
+            .exchange()
+            .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    @DisplayName("Unauthenticated route profiles get unauthenticated")
+    void test3() {
+
+        String url = "/profile/user123";
+        stubFor(WireMock.get(WireMock.urlEqualTo(url))
+            .willReturn(WireMock.aResponse().withStatus(200)));
+
+        webClient
+            .mutateWith(mockJwt())
+            .get()
+            .uri(url)
+            .exchange()
+            .expectStatus().isOk();
+    }
+}
