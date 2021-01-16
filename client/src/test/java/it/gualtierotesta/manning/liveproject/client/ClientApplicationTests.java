a3e49ae29ba4422f717c3566003b3c4b3566d55a
Gualtiero Testa <gualtiero.testa@factor-y.com>
2021-01-16 15:00:18 +0100

    Initial commit for the Client application

---
+++ client/src/test/java/it/gualtierotesta/manning/liveproject/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gualtierotesta.manning.liveproject.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
